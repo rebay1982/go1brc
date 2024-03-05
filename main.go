0b46d7c7c14cd55989290620532bf3d28016a71f
Reda Baydoun <reda.baydoun@gmail.com>
2024-03-04 22:14:48 -0500

    feat: revert main.go to only telemetry

--- main.go
+++ main.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"fmt"	
 	"log"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
 
-type stationData struct {
+type measurement struct {
 	min, max, sum float64
 	count int
-}
-type measurement struct	{
-	name string
-	temp float64
 }
 
 type timing struct {
@@ -30,83 +25,8 @@ func (t timing) GetElapsedMS() time.Duration {
 	return t.elapsed// /time.Millisecond
 }
 
-func workGen(scanner *bufio.Scanner) <- chan string {
-	genOut := make(chan string)
-	go func() {
-		for scanner.Scan() {
-			genOut <- scanner.Text()
-		}
-		close(genOut)
-	}()
-	return genOut
-}
-
-// Line parser
-func measurementParser(in <- chan string) <- chan measurement {
-	parseOut := make(chan measurement)
-	go func() {
-		for line := range in {
-			splits := strings.Split(line, ";")
-
-			temp, _ := strconv.ParseFloat(splits[1], 64)
-			m := measurement{name: splits[0], temp: temp}
-			parseOut <-m
-		}
-		close(parseOut)
-	}()
-	return parseOut
-}
-
-func readFile(file *os.File, numParsers int) map[string]*stationData {
-	// Work generator -- generates lines that need to be processed
-	genOut := workGen(bufio.NewScanner(file))
-
-	// Parsers -- receives lines from the generators and parse the work.
-	parseOut := []<-chan measurement{}
-	for i := 0; i < numParsers; i++ {
-		parseOut = append(parseOut, measurementParser(genOut))
-	}
-
-	// Everything is lined up now, all you have to do is implement the merge sink and do line 79 processing on the 
-	// parser output
-	var wg sync.WaitGroup
-	aggOutput := make(chan measurement)
-	sink := func(in <- chan measurement)	{
-		for m := range in {
-			aggOutput <- m
-		}
-		wg.Done()
-	}
-	wg.Add(numParsers)
-
-	for _, c := range parseOut {
-		go sink(c)	
-	}
-
-	go func() {
-		wg.Wait()
-		close(aggOutput)
-	}()
-
-	results := make(map[string]*stationData)
-	for m := range aggOutput {
-		station := m.name
-		temp := m.temp
-		r, ok := results[station]
-		if !ok {
-			r = &stationData{min: temp, max: temp, sum: temp}
-			results[station] = r
-		} else {
-			r.min = min(r.min, temp)
-			r.max = max(r.max, temp)
-			r.sum += temp
-		}
-		r.count++
-	}
-	return results
-}
-
 func main() {
+
 	filename := "measurements.txt"
 	file, err := os.Open(filename)
 
@@ -116,8 +36,33 @@ func main() {
 	}
 	defer file.Close()
 
+	results := make(map[string]*measurement)
+	scanner := bufio.NewScanner(file)
+
 	scanTiming := timing{start: time.Now()}
-	results := readFile(file, 5)
+	for scanner.Scan() {
+		line := scanner.Text()
+		splits := strings.Split(line, ";")
+
+		station := splits[0]
+		temp, err := strconv.ParseFloat(splits[1], 64)
+		if err != nil {
+			log.Fatalf("Failed to parse float [%s], err = %v\n", splits[1], err)
+			os.Exit(1)
+		}
+
+		r, ok := results[station]
+		if !ok {
+			r = &measurement{min: temp, max: temp, sum: temp}
+			results[station] = r
+		} else {
+			r.min = min(r.min, temp)
+			r.max = max(r.max, temp)
+			r.sum += temp
+		}
+
+		r.count++
+	}
 	scanTiming.elapsed = time.Since(scanTiming.start)
 
 	// Sort stuff
